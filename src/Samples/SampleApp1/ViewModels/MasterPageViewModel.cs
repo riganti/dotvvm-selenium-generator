782a72afbb8c3cd631c0da0dc22e9f6745c47989
Filip Kalous <filip.kalous@gmail.com>
2019-02-16 13:51:40 +0100

    Project setup

---
+++ src/Samples/SampleApp1/ViewModels/MasterPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DotVVM.Framework.ViewModel;
+
+namespace SampleApp1.ViewModels
+{
+    public class MasterPageViewModel : DotvvmViewModelBase
+    {
+    }
+}
